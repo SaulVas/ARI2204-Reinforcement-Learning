4808ec120c59cc5fad77d60808cb35b30b82a3f5
SaulVas <saul.vassallo.22@um.edu.mt>
2024-05-11 19:01:22 +0200

    implemented q learning

--- game_structure/Blackjack.py
+++ game_structure/Blackjack.py
@@ -36,7 +36,6 @@ class BlackJack:
 
     def play_round_MC(self, agent):
         self.initialise_round(agent)
-        reward = DRAW
 
         # deal cards
         for _ in range(2):
@@ -52,29 +51,32 @@ class BlackJack:
 
             self.agent_hand.add_card(self.deck.draw_card())
             if self._is_bust(self.agent_hand):
-                reward = LOSS
                 break
 
         # dealer actions
-        while True and reward != LOSS:
+        while not self._is_bust(self.agent_hand):
             self.dealer.set_hand(self.dealer_hand)
             if self.dealer.get_action() == STAND:
                 break
 
             self.dealer_hand.add_card(self.deck.draw_card())
             if self._is_bust(self.dealer_hand):
-                reward = WIN
                 break
 
         # calculate the winner
         agent_value = self.agent_hand.calculate_hand_value()
         dealer_value = self.dealer_hand.calculate_hand_value()
 
-        if agent_value <= 21 and dealer_value <= 21:
-            if agent_value > dealer_value:
-                reward = WIN
-            elif agent_value < dealer_value:
-                reward = LOSS
+        if agent_value > 21:
+            reward = LOSS
+        elif dealer_value > 21:
+            reward = WIN
+        elif agent_value > dealer_value:
+            reward = WIN
+        elif agent_value < dealer_value:
+            reward = LOSS
+        else:
+            reward = DRAW
 
         self.agent.update_q_values(agent.get_episode(), reward)
         return reward
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from game_structure.Blackjack import BlackJack
 # from agents.UserAgent import UserAgent
 from agents.MonteCarlo import MonteCarloControl
 from agents.Sarsa import SarsaControl
-from agents.AgentABC import WIN, DRAW
+from agents.QLearning import QlearningControl
+from agents.AgentABC import WIN, LOSS
 
 def monte_carlo_round(agent, game, tally):
     reward = game.play_round_MC(agent)
@@ -15,7 +16,7 @@ def sarsa_q_round(agent, game, tally):
 def update_reward_tally(reward, tally):
     if reward == WIN:
         tally[0] += 1
-    elif reward == DRAW:
+    elif reward == LOSS:
         tally[1] += 1
     else:
         tally[2] += 1
@@ -36,6 +37,11 @@ if __name__ == "__main__":
     SARSA2 = SarsaControl(2)
     SARSA3 = SarsaControl(3)
 
+    Q0 = QlearningControl(0)
+    Q1 = QlearningControl(1)
+    Q2 = QlearningControl(2)
+    Q3 = QlearningControl(3)
+
 
     score = {
         "MCES": [0, 0, 0],
@@ -46,17 +52,25 @@ if __name__ == "__main__":
         "SARSA1": [0, 0, 0],
         "SARSA2": [0, 0, 0],
         "SARSA3": [0, 0, 0],
+        "QLearning0": [0, 0, 0],
+        "QLearning1": [0, 0, 0],
+        "QLearning2": [0, 0, 0],
+        "QLearning3": [0, 0, 0],
     }
 
-    for _ in range(100000):
+    for _ in range(10000000):
         monte_carlo_round(MCES, blackjack, score['MCES'])
         monte_carlo_round(MC0, blackjack, score['MC0'])
         monte_carlo_round(MC1, blackjack, score['MC1'])
         monte_carlo_round(MC2, blackjack, score['MC2'])
-        sarsa_q_round(SARSA0, blackjack, score['SARSA0'])
-        sarsa_q_round(SARSA1, blackjack, score['SARSA1'])
-        sarsa_q_round(SARSA2, blackjack, score['SARSA2'])
-        sarsa_q_round(SARSA3, blackjack, score['SARSA3'])
+        # sarsa_q_round(SARSA0, blackjack, score['SARSA0'])
+        # sarsa_q_round(SARSA1, blackjack, score['SARSA1'])
+        # sarsa_q_round(SARSA2, blackjack, score['SARSA2'])
+        # sarsa_q_round(SARSA3, blackjack, score['SARSA3'])
+        # sarsa_q_round(Q0, blackjack, score['QLearning0'])
+        # sarsa_q_round(Q1, blackjack, score['QLearning1'])
+        # sarsa_q_round(Q3, blackjack, score['QLearning2'])
+        # sarsa_q_round(Q3, blackjack, score['QLearning3'])
 
 
     print(f"MCES: Wins: {score['MCES'][0]}, Draws: {score['MCES'][1]}, Losses: {score["MCES"][2]}")
@@ -68,3 +82,8 @@ if __name__ == "__main__":
     print(f"SARSA1: Wins: {score['SARSA1'][0]}, Draws: {score['SARSA1'][1]}, Losses: {score["SARSA1"][2]}")
     print(f"SARSA2: Wins: {score['SARSA2'][0]}, Draws: {score['SARSA2'][1]}, Losses: {score["SARSA2"][2]}")
     print(f"SARSA3: Wins: {score['SARSA3'][0]}, Draws: {score['SARSA3'][1]}, Losses: {score["SARSA3"][2]}")
+    
+    print(f"QLearning0: Wins: {score['QLearning0'][0]}, Draws: {score['QLearning0'][1]}, Losses: {score["QLearning0"][2]}")
+    print(f"QLearning1: Wins: {score['QLearning1'][0]}, Draws: {score['QLearning1'][1]}, Losses: {score["QLearning1"][2]}")
+    print(f"QLearning2: Wins: {score['QLearning2'][0]}, Draws: {score['QLearning2'][1]}, Losses: {score["QLearning2"][2]}")
+    print(f"QLearning3: Wins: {score['QLearning3'][0]}, Draws: {score['QLearning3'][1]}, Losses: {score["QLearning3"][2]}")
