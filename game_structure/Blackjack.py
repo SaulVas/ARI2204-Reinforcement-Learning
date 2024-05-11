d2676b873c0f800b3b7ff1a3e7f4a101c1476acc
SaulVas <saul.vassallo.22@um.edu.mt>
2024-05-11 15:29:59 +0200

    bj methods for specific rl techniques

--- game_structure/Blackjack.py
+++ game_structure/Blackjack.py
@@ -1,4 +1,4 @@
-from agents.AgentABC import WIN, LOST, DRAW, STAND
+from agents.AgentABC import WIN, LOSS, DRAW, STAND
 from game_structure.Deck import Deck
 from agents.Dealer import Dealer
 from game_structure.Hand import Hand
@@ -26,7 +26,7 @@ class BlackJack:
     def _set_agent(self, agent):
         self.agent = agent
 
-    def play_round(self, agent):
+    def play_round_MC(self, agent):
         self.deck.reset()
         self.deck.shuffle()
         self._set_agent(agent)
@@ -47,7 +47,7 @@ class BlackJack:
 
             self.agent_hand.add_card(self.deck.draw_card())
             if self._is_bust(self.agent_hand):
-                return LOST
+                return LOSS
 
         # dealer actions
         while True:
@@ -65,10 +65,60 @@ class BlackJack:
 
         if agent_value > dealer_value:
             return WIN
-
         if agent_value < dealer_value:
-            return LOST
-
+            return LOSS
         return DRAW
 
-    
+    def play_round_SARSA_Q(self, agent):
+        self.deck.reset()
+        self.deck.shuffle()
+        self._set_agent(agent)
+        self.agent_hand.reset_hand()
+        self.dealer_hand.reset_hand()
+
+        # deal cards
+        for _ in range(2):
+            self.agent_hand.add_card(self.deck.draw_card())
+            self.dealer_hand.add_card(self.deck.draw_card())
+
+        # agent actions
+        while True:
+            self._set_state(self.agent_hand, self.dealer_hand)
+            self.agent.set_state(self.state)
+            if self.agent.get_action() == STAND:
+                break
+
+            self.agent_hand.add_card(self.deck.draw_card())
+            if self._is_bust(self.agent_hand):
+                agent.update_q_values(agent.get_state_action_value(-1), None, LOSS, True)
+                return LOSS
+            if len(agent.get_episode()) == 2:
+                agent.update_q_values(agent.get_state_action_value(-2),
+                                      agent.get_state_action_value(-1), 0, False)
+                agent.remove_state_action_value(0)
+
+
+        # dealer actions
+        while True:
+            self.dealer.set_hand(self.dealer_hand)
+            if self.dealer.get_action() == STAND:
+                break
+
+            self.dealer_hand.add_card(self.deck.draw_card())
+            if self._is_bust(self.dealer_hand):
+                return WIN
+
+        # calculate the winner
+        agent_value = self.agent_hand.calculate_hand_value()
+        dealer_value = self.dealer_hand.calculate_hand_value()
+
+        if agent_value > dealer_value:
+            # Player gets blackjack
+            if len(agent.get_episode() == 1):
+                agent.update_q_values(agent.get_state_action_value(-1), None, WIN, True)
+            return WIN
+
+        if agent_value < dealer_value:
+            return LOSS
+
+        return DRAW
